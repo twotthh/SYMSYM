9320a54378347cd3763a11d969b3f3e9f3610dde
Yedam <990624yd@naver.com>
2026-07-03 09:01:57 +0900

    Shodan API

---
+++ backend/collectors/shodan.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("SHODAN_API_KEY")
+
+
+def get_host(ip: str):
+    """
+    특정 IP의 정보를 조회
+    """
+
+    url = f"https://api.shodan.io/shodan/host/{ip}"
+
+    params = {
+        "key": API_KEY
+    }
+
+    response = requests.get(
+    url,
+    params=params,
+    timeout=10
+)
+
+    return response
+
+
+if __name__ == "__main__":
+
+    response = get_host("8.8.8.8")
+
+    print("Status:", response.status_code)
+
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(response.text)
